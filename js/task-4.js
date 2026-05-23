ae995c9b5666dd0e2d72cf37dbf8fc6db31dc500
Anastasiia Kasianova <n.kostik21@gmail.com>
2026-05-23 11:41:33 +0200

    add index file

--- js/task-4.js
+++ js/task-4.js
@@ -10,7 +10,7 @@ function getShippingCost(country) {
             return `Shipping to Australia will cost 170 credits`;
             break;
         case 'Jamaica':
-            return `Shipping to Jamaica will cost Jamaica credits`;
+            return `Shipping to Jamaica will cost 120 credits`;
             break;
         default:
             return `Sorry, there is no delivery to your country`;
